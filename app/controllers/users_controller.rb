7d42689ccec2ca18229ca8eadd364954d0e8d8a8
johnsonsirv <johnsonsirv@gmail.com>
2019-07-11 00:15:36 +0000

    added the GET action methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+	
+	def index
+		@name = 'Call to /index action method'
+	end
+	
+	def show
+		@name = 'Call to /show action method'
+	end
+	
+	def new
+		@name = 'Call to /new action method'
+	end
+	
+	def edit
+		@name = 'Call to /edit action method'
+	end
+	
 end
